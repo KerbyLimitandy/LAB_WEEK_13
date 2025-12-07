613934ec0cc947d05c2d554659bf4f363cc9f39d
KerbyLimitandy <kerby5677@gmail.com>
2025-12-07 23:54:41 +0700

    Commit No. 3 – update to WorkManager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     kapt(libs.androidx.room.compiler)
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
